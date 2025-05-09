7a2bf652a0f1ad10fddc16e9ee90a74e57d9282a
Vladislav <tekuzar@mail,ru>
2025-05-09 22:27:00 +0300

    DynamicRulesApi added!

--- src/main/java/pro/sky/recommendation/system/entity/DynamicRule.java
+++ src/main/java/pro/sky/recommendation/system/entity/DynamicRule.java
@@ -1,6 +1,8 @@
 package pro.sky.recommendation.system.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,8 +11,13 @@ import java.util.UUID;
 public class DynamicRule {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
 
     @Column(name = "product_name", nullable = false)
     private String productName;
@@ -21,18 +28,29 @@ public class DynamicRule {
     @Column(name = "product_text", nullable = false)
     private String productText;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "rule_id")
-    private List<RuleQuery> queries;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "rule_id", nullable = false)
+    private List<RuleQuery> rule;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getProductName() { return productName; }
-    public void setProductName(String productName) { this.productName = productName; }
-    public UUID getProductId() { return productId; }
-    public void setProductId(UUID productId) { this.productId = productId; }
-    public String getProductText() { return productText; }
-    public void setProductText(String productText) { this.productText = productText; }
-    public List<RuleQuery> getQueries() { return queries; }
-    public void setQueries(List<RuleQuery> queries) { this.queries = queries; }
+    public UUID getId() {
+        return id; }
+    public void setId(UUID id) {
+        this.id = id; }
+    public String getProductName() {
+        return productName; }
+    public void setProductName(String productName) {
+        this.productName = productName; }
+    public UUID getProductId() {
+        return productId; }
+    public void setProductId(UUID productId) {
+        this.productId = productId; }
+    public String getProductText() {
+        return productText; }
+    public void setProductText(String productText) {
+        this.productText = productText; }
+    public List<RuleQuery> getRule() {
+        return rule; }
+    public void setRule(List<RuleQuery> rule) {
+        this.rule = rule; }
 }
+
